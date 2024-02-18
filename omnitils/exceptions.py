695ce0ce4b3b34a1fd5b7a0555b8067a89be0cd4
Investigamer <freethoughtleft@gmail.com>
2024-02-18 04:29:52 -0600

    feat(exceptions): New util module 'exceptions.py' for dealing with exceptions
    Signed-off-by: Investigamer <freethoughtleft@gmail.com>

---
+++ omnitils/exceptions.py
@@ -0,0 +1,56 @@
+"""
+* Exception Utilities
+* Generalized utilities for dealing with exceptions.
+* Copyright (c) Hexproof Systems <hexproofsystems@gmail.com>
+* LICENSE: Mozilla Public License 2.0
+"""
+# Standard Library Imports
+from contextlib import suppress
+from logging import getLogger
+from typing import Callable, Any, Optional
+
+"""
+* Utility Decorators
+"""
+
+
+def log_on_exception(logr: Any = None) -> Callable:
+    """Decorator to log any exception that occurs.
+
+    Args:
+        logr: Logger object to output any exception messages.
+
+    Returns:
+        Wrapped function.
+    """
+    logr = logr or getLogger()
+
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            # Final exception catch
+            try:
+                return func(*args, **kwargs)
+            except Exception as e:
+                logr.log_exception(e)
+                raise e
+        return wrapper
+    return decorator
+
+
+def return_on_exception(response: Optional[Any] = None) -> Callable:
+    """Decorator to handle any exception and return appropriate failure value.
+
+    Args:
+        response: Value to return if an exception occurs.
+
+    Returns:
+        Wrapped function.
+    """
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            # Final exception catch
+            with suppress(Exception):
+                return func(*args, **kwargs)
+            return response
+        return wrapper
+    return decorator
