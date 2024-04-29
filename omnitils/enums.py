37c680d58beff7a23dd343a5a74f2664fde494cb
Chris <hexproofsystems@gmail.com>
2024-04-28 21:29:27 -0500

    fix(enums): Correct idiosyncrasies with StrEnum class, rewrite StrEnumMeta and rename to more generic EnumCollection, introduce experimental URLEnum (enum class for yarl.URL objects)
    Signed-off-by: Chris <hexproofsystems@gmail.com>

--- omnitils/enums.py
+++ omnitils/enums.py
@@ -7,20 +7,39 @@
 # Standard Library Imports
 from enum import Enum, EnumMeta
 from functools import cached_property
+from typing import Iterator, Any
+
+# Local Imports
+from omnitils.properties import default_prop
+
+"""
+* Enum Meta-classes
+"""
+
+
+class EnumCollection(EnumMeta):
+    """Metaclass for StrEnum."""
+
+    def __iter__(cls) -> Iterator[Any]:
+        """Iterate over the Enum collection."""
+        for n in cls._member_map_.values():
+            yield n.value
+
+    def __contains__(cls, item: Any) -> bool:
+        """Check if item is contained in Enum collection."""
+        return bool(item in cls._member_map_.values())
+
+    @default_prop
+    def Default(cls) -> Any:
+        return cls._value2member_map_.__iter__().__next__()
+
 
 """
 * String Enum Classes
 """
 
 
-class StrEnumMeta(EnumMeta):
-    """Metaclass for StrEnum."""
-
-    def __contains__(cls, item: str) -> bool:
-        return item in cls._value2member_map_
-
-
-class StrEnum(str, Enum, metaclass=StrEnumMeta):
+class StrEnum(str, Enum, metaclass=EnumCollection):
     """Enum where the value is always a string."""
 
     def __str__(self) -> str:
@@ -30,6 +49,22 @@ class StrEnum(str, Enum, metaclass=StrEnumMeta):
     def value(self) -> str:
         return str(self._value_)
 
-    @cached_property
-    def Default(self) -> str:
-        return "default"
+
+class URLEnum(Enum, metaclass=EnumCollection):
+    """Enum where the value is always a URL object with an HTTPS scheme.
+
+    Todo:
+        Robustly test this Enum class.
+    """
+
+    def __str__(self):
+        return str(self.value)
+
+    def __truediv__(self, value):
+        return self.value / value
+
+    def __getattr__(self, item: str) -> Any:
+        """Access anything except _value_, value, and __contains__ from the URL object."""
+        if item not in ['_value_', 'value', '__contains__']:
+            return self.value.__getattribute__(item)
+        return self.__getattribute__(item)
